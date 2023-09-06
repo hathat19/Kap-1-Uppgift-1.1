3b6c49c140c07ebf1d51fbfa7f5c055d4d6db29d
humy06001 <humy06001@E-PW0234KS>
2023-09-06 12:31:38 +0200

    Add project files.

---
+++ Kap 1 Uppgift 1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Skriver ut den först raden i Console.
+            Console.WriteLine("Du har lärt dig att skriva tecken som \" och \\."); //Ny rad med ny text. 
+
+        }
+    }
+
+}
